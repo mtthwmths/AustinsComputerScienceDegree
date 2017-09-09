88311f329b34ad5b5404b8210e55368db78e62c9
Matt M <mtthwmths@gmail.com>
2017-09-08 21:11:09 -0500

    Update WhileLoops.py
    added a comment about input casting so I don't forget again. :D

--- WhileLoops.py
+++ WhileLoops.py
@@ -1,10 +1,12 @@
 # Code I wrote teaching my cousin about 
-# while loops in Python
+# while loops in Python.
+
 userInput = 3
 
 while True:
 	print("menu")
 	print("1 Celsius, 2 Farenheit, 3 exit")
+	#don't forget to cast inputs if you want anything other than a string
 	userInput = int(input("Enter Choice: "))
 	if userInput == 1:
 		print("Celsius is for communists")
